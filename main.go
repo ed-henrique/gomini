48202808277f5d4a37eb76f5d749c2e7c21544a1
Eduardo Henrique Freire Machado <edu.hen.fm@gmail.com>
2025-01-02 11:44:39 -0400

    feat: gets absolute URI from Authority (minus Port)

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"strings"
+)
+
+const (
+	URIScheme      = "gemini"
+	URIDefaultPort = 1965
+	URILengthLimit = 1024
+	CRLF           = "\r\n"
+)
+
+func getAbsoluteURI(authority string) string {
+	authority = strings.TrimSuffix(authority, "/")
+	return fmt.Sprintf("%s://%s:%d/%s", URIScheme, authority, URIDefaultPort, CRLF)
+}
 
 func main() {
-	fmt.Println("Hey")
+	uriAuthority := flag.String("path", "", "sets the path to send the request")
+	flag.Parse()
+
+	fmt.Print(getAbsoluteURI(*uriAuthority))
 }
---
+++ main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import "testing"
+
+func TestGetAbsoluteURI(t *testing.T) {
+	testTable := []struct {
+		name      string
+		authority string
+		expected  string
+	}{
+		{
+			name:      "authority without / suffix",
+			authority: "a",
+			expected:  "gemini://a:1965/\r\n",
+		},
+		{
+			name:      "authority with / suffix",
+			authority: "a/",
+			expected:  "gemini://a:1965/\r\n",
+		},
+	}
+
+	for _, tt := range testTable {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := getAbsoluteURI(tt.authority)
+			if got != tt.expected {
+				t.Errorf("got %s expected %s", got, tt.expected)
+			}
+		})
+	}
+}
